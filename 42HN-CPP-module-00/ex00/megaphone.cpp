0008000832916da29dfc6b99672270f55ace10cb
mratke <mratke@student.42heilbronn.de>
2025-05-13 19:42:07 +0200

    figured out about range loops and used it

--- 42HN-CPP-module-00/ex00/megaphone.cpp
+++ 42HN-CPP-module-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 00:02:46 by mratke            #+#    #+#             */
-/*   Updated: 2025/05/13 01:21:38 by mratke           ###   ########.fr       */
+/*   Updated: 2025/05/13 19:36:18 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ int main(int argc, char **argv) {
   if (argc == 1) {
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
   } else {
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; ++i) {
       std::string str = argv[i];
-      for (size_t j = 0; j < str.length(); j++) {
-        str[j] = std::toupper(str[j]);
+      for (auto& c : str) {
+        c = std::toupper(c);
       }
       std::cout << str;
     }
     std::cout << "\n";
   }
+  return 0;
 }
