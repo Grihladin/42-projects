96f52b4808513069c1a7f4a1a6d3d353928156b6
mratke <mratke@student.42heilbronn.de>
2025-06-17 15:39:57 +0200

    finished ex01

--- 42HN-CPP-module-00/ex01/phoneBook.cpp
+++ 42HN-CPP-module-00/ex01/phoneBook.cpp
@@ -6,25 +6,33 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:52:04 by mratke            #+#    #+#             */
-/*   Updated: 2025/05/13 21:13:42 by mratke           ###   ########.fr       */
+/*   Updated: 2025/06/17 15:39:25 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cctype>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <string>
+
+std::string formatStr(std::string &str) {
+  if (str.length() > 10)
+    return (str.substr(0, 9) + ".");
+  return (str);
+}
 
 class Contact {
 private:
-  int phoneNumber;
+  std::string phoneNumber;
   std::string nickname;
   std::string firstName;
   std::string lastName;
   std::string secret;
 
 public:
-  Contact(int n, std::string nick, std::string first, std::string second,
-          std::string darkSecret) {
+  Contact(std::string n, std::string nick, std::string first,
+          std::string second, std::string darkSecret) {
     phoneNumber = n;
     nickname = nick;
     firstName = first;
@@ -33,42 +41,116 @@ public:
   }
 
   Contact() {
-    phoneNumber = 0;
+    phoneNumber = "";
     nickname = "";
     firstName = "";
     lastName = "";
     secret = "";
   }
 
-  ~Contact() {
-    phoneNumber = 0;
-    nickname = "";
-    firstName = "";
-    lastName = "";
-    secret = "";
-  }
+  std::string getPhoneNumber() { return (phoneNumber); }
+  std::string getNickname() { return (nickname); }
+  std::string getFirstName() { return (firstName); }
+  std::string getLastName() { return (lastName); }
+  std::string getSecret() { return (secret); }
 };
 
 class PhoneBook {
 private:
-  Contact contacts[9];
+  Contact contacts[8];
   int currentIndex;
 
 public:
   PhoneBook() { currentIndex = 0; }
 
-  void initPhoneBook() { //improove this part no need to init i have constuctor. here need addcontact
-    for (auto &obj : contacts) {
-      obj = Contact();
+  void addContact(Contact &newContact) {
+    contacts[currentIndex % 8] = newContact;
+    currentIndex++;
+  }
+
+  void displayAllContacts() {
+    std::string tmp;
+    std::cout << std::left;
+    std::cout << "----------+----------+----------+----------+" << '\n';
+    std::cout << std::setw(10) << "INDEX" << "|";
+    std::cout << std::setw(10) << "FIRST NAME" << "|";
+    std::cout << std::setw(10) << "LAST NAME" << "|";
+    std::cout << std::setw(10) << "NICKNAME" << "|" << "\n";
+    std::cout << "----------+----------+----------+----------+" << '\n';
+    for (int i = 0; i < 8; i++) {
+      if (!contacts[i].getPhoneNumber().empty()) {
+        std::cout << std::setw(10) << i << "|";
+        tmp = contacts[i].getFirstName();
+        std::cout << std::setw(10) << formatStr(tmp) << "|";
+        tmp = contacts[i].getLastName();
+        std::cout << std::setw(10) << formatStr(tmp) << "|";
+        tmp = contacts[i].getNickname();
+        std::cout << std::setw(10) << formatStr(tmp) << "|" << '\n';
+        std::cout << "----------+----------+----------+----------+" << '\n';
+      }
+    }
+  }
+
+  void displayContact(int index) {
+    if (index >= 0 && index < 8 && !contacts[index].getPhoneNumber().empty()) {
+      std::cout << "First Name: " << contacts[index].getFirstName()
+                << std::endl;
+      std::cout << "Last Name: " << contacts[index].getLastName() << '\n';
+      std::cout << "Nickname: " << contacts[index].getNickname() << '\n';
+      std::cout << "Phone Number: " << contacts[index].getPhoneNumber() << '\n';
+      std::cout << "Darkest Secret: " << contacts[index].getSecret() << '\n';
+    } else {
+      std::cout << "Invalid index or no contact at this position." << '\n';
     }
   }
 };
 
-Contact createContact() {}
+bool isValidNumber(std::string number) {
+  for (auto &c : number) {
+    if (!std::isdigit(c) && c != '+' && c != '-' && c != ' ' && c != '(' &&
+        c != ')') {
+      return (false);
+    }
+  }
+  return (true);
+}
+
+Contact createContact() {
+  std::string phoneNumber, nickname, firstName, lastName, secret;
+  bool validPhoneNumber = false;
+
+  while (!validPhoneNumber) {
+    std::cout << "Enter phone number: ";
+    std::getline(std::cin, phoneNumber);
+    if (!isValidNumber(phoneNumber))
+      std::cout << "Phone number can contain digits/+/-/)/), please enter "
+                   "correct format\n";
+    else
+      validPhoneNumber = true;
+  }
+
+  std::cout << "Enter nickname: ";
+  std::getline(std::cin, nickname);
+
+  std::cout << "Enter first name: ";
+  std::getline(std::cin, firstName);
+
+  std::cout << "Enter last name: ";
+  std::getline(std::cin, lastName);
+
+  std::cout << "Enter darkest secret: ";
+  std::getline(std::cin, secret);
+
+  return (Contact(phoneNumber, nickname, firstName, lastName, secret));
+}
 
 int main(int argc, char **argv) {
-  int phoneNumber;
-  ;
+  std::string phoneNumber;
+  std::string command;
+  PhoneBook book;
+  std::string str;
+  Contact newContact;
+  int index = -1;
 
   while (1) {
     std::cout << "Enter command (ADD, SEARCH, EXIT): ";
@@ -76,7 +158,21 @@ int main(int argc, char **argv) {
     if (command == "EXIT")
       break;
     else if (command == "ADD") {
-      std::cout << "Enter phone number: " std::getline(std::cin, phoneNumber)
+      newContact = createContact();
+      book.addContact(newContact);
+    } else if (command == "SEARCH") {
+      book.displayAllContacts();
+      while (!(index >= 0 && index <= 7)) {
+        std::cout << "Choose contact (0 to 7): ";
+        std::getline(std::cin, str);
+        if (str.length() == 1 && std::isdigit(str[0])) {
+          index = str[0] - '0';
+          if (!(index >= 0 && index <= 7))
+            std::cout << "Invalid index.\n";
+        } else
+          std::cout << "Enter a singe digit. \n";
+      }
+      book.displayContact(index);
     }
   }
 }
