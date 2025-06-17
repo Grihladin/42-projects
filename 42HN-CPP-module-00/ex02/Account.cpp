fd5d255c8f9a3c3a3aae1da3af7454c3dd9e3dfe
mratke <mratke@student.42heilbronn.de>
2025-06-17 19:48:54 +0200

    added constructor and destructor

--- 42HN-CPP-module-00/ex02/Account.cpp
+++ 42HN-CPP-module-00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 23:59:54 by mratke            #+#    #+#             */
-/*   Updated: 2025/06/17 17:24:12 by mratke           ###   ########.fr       */
+/*   Updated: 2025/06/17 19:47:35 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ int Account::getNbDeposits(void) { return _totalNbDeposits; }
 
 int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 
+void Account::_displayTimestamp(void) {
+  std::time_t t = std::time(0);
+  std::tm *now = std::localtime(&t);
+  std::cout << "[" << std::put_time(now, "%Y-%m-%d_%H:%M:%S") << "]\n";
+}
+
 void Account::displayAccountsInfos(void) {
   _displayTimestamp();
   std::cout << "Account number: " << getNbAccounts() << '\n';
@@ -36,9 +42,21 @@ void Account::displayAccountsInfos(void) {
   std::cout << "Withdrawals: " << getNbWithdrawals() << '\n';
 }
 
-void Account::_displayTimestamp(void) {
-  std::time_t t = std::time(0);
-  std::tm *now = std::localtime(&t);
-  std::cout << "[" << std::put_time(now, "%Y-%m-%d_%H:%M:%S") << "]\n";
+Account::Account(int initial_deposit)
+    : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0),
+      _nbWithdrawals(0) {
+  _nbAccounts++;
+  _totalAmount += initial_deposit;
+
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";"
+            << "amount:" << _amount << ";"
+            << "created" << std::endl;
 }
 
+Account::~Account(void) {
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";"
+            << "amount:" << _amount << ";"
+            << "closed" << std::endl;
+}
